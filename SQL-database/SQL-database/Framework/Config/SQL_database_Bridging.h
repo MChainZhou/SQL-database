d31bee0b6143a5d8d8ded8e60989f73938fb39f3
MChainZhou <18395891212@163.com>
2017-08-14 17:37:14 +0800

    桥接OC的sqlite数据库

---
+++ SQL-database/SQL-database/Framework/Config/SQL_database_Bridging.h
@@ -0,0 +1,14 @@
+//
+//  SQL_database_Bridging.h
+//  SQL-database
+//
+//  Created by apple on 2017/8/14.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#ifndef SQL_database_Bridging_h
+#define SQL_database_Bridging_h
+
+#import "sqlite3.h"
+
+#endif /* SQL_database_Bridging_h */
---
+++ SQL-database/SQL-database/Framework/Core/DBConnection.swift
@@ -0,0 +1,9 @@
+//
+//  DBConnection.swift
+//  SQL-database
+//
+//  Created by apple on 2017/8/14.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+import Foundation
